1601a6d6f709ce4d1856f4b6d46df530f3765289
johdirr <johannesdirr@me.com>
2016-07-29 19:25:31 +0200

    put all color function into a class

--- app/main.js
+++ app/main.js
@@ -1,34 +1,37 @@
 
-function getRandomColorRgba(opacity) {
-  var r = Math.floor(Math.random() * 255),
-      g = Math.floor(Math.random() * 255),
-      b = Math.floor(Math.random() * 255),
-      a = opacity || 1
-
-  return `rgba(${r}, ${g}, ${b}, ${a})`
-}
-
-
-function setBodyBack(color) {
-  document.querySelector('body').style.backgroundColor = color
-}
-
-
-function setChildrenColor(color) {
-  const children = document.querySelector('body').children
-  const links = document.querySelectorAll('a')
-
-  for (let child of children) {
-    child.style.color = color
+class Colorizer {
+  constructor(tagName = 'body') {
+    this._root = document.querySelector(tagName)
+    this._children = document.querySelectorAll(`${tagName} *`)
   }
 
-  for (let link of links) {
-    link.style.color = color
+
+  getRandomColorRgba(opacity = 1) {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    const a = opacity
+
+    return `rgba(${r}, ${g}, ${b}, ${a})`
+  }
+
+
+  setBackgroundColor(color = this.getRandomColorRgba()) {
+    this._root.style.backgroundColor = color
+  }
+
+
+  setChildrenColor(color = this.getRandomColorRgba()) {
+    for (let child of this._children) {
+      child.style.color = color
+    }
   }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => setBodyBack(getRandomColorRgba()), 1000)
-  setInterval(() => setChildrenColor(getRandomColorRgba()), 1200)
+  const colorizer = new Colorizer('body');
+
+  setInterval(() => colorizer.setBackgroundColor(), 1000)
+  setInterval(() => colorizer.setChildrenColor(), 1200)
 });
